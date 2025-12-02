c91c84fd5efa8c5d74df0eef0c24111a4caf94e4
Daniel <chung@qunasys.com>
2025-12-02 20:28:35 +0800

    docstring

--- quri-parts/packages/qsub/quri_parts/qsub/resolve/resolve.py
+++ quri-parts/packages/qsub/quri_parts/qsub/resolve/resolve.py
@@ -112,8 +112,8 @@ def default_repository() -> SubRepository:
 
 
 class CompositeSubRepository(SubRepositoryProtocol):
-    """A :class:`SubRepositoryProtocol` that holds the root repo and a sequence
-    of additional `SubRepository`s."""
+    """A :class:`SubRepositoryProtocol` that holds the parent repo and an additional
+    child `SubRepository`."""
 
     def __init__(self, parent_repo: SubRepositoryProtocol, child_repo: SubRepository):
         self.parent_repo = parent_repo
