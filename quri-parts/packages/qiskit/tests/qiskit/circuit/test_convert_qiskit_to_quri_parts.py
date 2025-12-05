ae800a32be3a2827c743d045f9694ca48752021a
Nils Wittemeier <nils.wittemeier@gmail.com>
2025-12-05 13:05:03 -0500

    Add test for qiskit to qulacs roundtrip

--- quri-parts/packages/qiskit/tests/qiskit/circuit/test_convert_qiskit_to_quri_parts.py
+++ quri-parts/packages/qiskit/tests/qiskit/circuit/test_convert_qiskit_to_quri_parts.py
@@ -8,8 +8,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from math import sqrt
-
 import numpy as np
 from qiskit import QuantumCircuit as QiskitCircuit
 from qiskit.circuit.library import UnitaryGate
