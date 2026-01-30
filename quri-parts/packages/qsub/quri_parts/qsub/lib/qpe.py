37fd90e591366c1039b01c318b5b913aebb4d1a5
Kazutaka Homma <special18fender@gmail.com>
2026-01-30 16:10:09 +0900

    fix comment

--- quri-parts/packages/qsub/quri_parts/qsub/lib/qpe.py
+++ quri-parts/packages/qsub/quri_parts/qsub/lib/qpe.py
@@ -25,7 +25,7 @@ from quri_parts.qsub.opsub import ParamUnitarySubDef, param_opsub
 from quri_parts.qsub.sub import SubBuilder
 
 warnings.warn(
-    "quri_algo.qsub.lib.qpe is deprecated. Please import with quri_algo.qsub.qpe instead.",
+    "quri_parts.qsub.lib.qpe is deprecated. Please import with quri_algo.qsub.qpe instead.",
     FutureWarning,
     stacklevel=2,
 )
