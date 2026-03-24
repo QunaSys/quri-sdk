082e01864ca6715398aa3cae57d4a8412644bfe5
Kazutaka Homma <special18fender@gmail.com>
2026-03-24 10:57:31 +0900

    Add total qubit count evaluator

--- quri-parts/packages/qsub/quri_parts/qsub/eval/qubitcount.py
+++ quri-parts/packages/qsub/quri_parts/qsub/eval/qubitcount.py
@@ -58,3 +58,46 @@ class AuxQubitCountEvaluatorHooks(EvaluatorHooks[int]):
         call_stack: list[SubId],
     ) -> None:
         ...
+
+
+class TotalQubitCountEvaluatorHooks(EvaluatorHooks[int]):
+    def __init__(self) -> None:
+        self.reset()
+
+    def reset(self) -> None:
+        self._total_qubit_count = 0
+        self._cache: dict[SubId, int] = defaultdict(int)
+
+    def result(self) -> int:
+        return self._total_qubit_count
+
+    def _merge_cache(self, caller_id: SubId, callee_id: SubId) -> None:
+        self._cache[caller_id] = max(self._cache[caller_id], self._cache[callee_id])
+
+    def enter_sub(
+        self,
+        sub: MachineSub,
+        qubits: Sequence[Qubit],
+        regs: Sequence[Register],
+        call_stack: list[SubId],
+    ) -> bool:
+        return sub.sub_id not in self._cache
+
+    def exit_sub(
+        self, sub: MachineSub, enter_sub: bool, call_stack: list[SubId]
+    ) -> None:
+        if enter_sub:
+            self._cache[sub.sub_id] += len(sub.aux_qubits)
+        if len(call_stack) > 1:
+            self._merge_cache(call_stack[-2], sub.sub_id)
+        else:
+            self._total_qubit_count = len(sub.qubits) + self._cache[sub.sub_id]
+
+    def primitive(
+        self,
+        mop: Primitive,
+        qubits: Sequence[Qubit],
+        regs: Sequence[Register],
+        call_stack: list[SubId],
+    ) -> None:
+        ...
--- quri-parts/packages/qsub/tests/eval/test_count.py
+++ quri-parts/packages/qsub/tests/eval/test_count.py
@@ -1,7 +1,10 @@
 from quri_parts.qsub.codegen import CodeGenerator
 from quri_parts.qsub.compile import compile_sub
 from quri_parts.qsub.eval.gatecount import TGateCountEvaluatorHooks
-from quri_parts.qsub.eval.qubitcount import AuxQubitCountEvaluatorHooks
+from quri_parts.qsub.eval.qubitcount import (
+    AuxQubitCountEvaluatorHooks,
+    TotalQubitCountEvaluatorHooks,
+)
 from quri_parts.qsub.evaluate import Evaluator
 from quri_parts.qsub.lib.std import MultiControlled, S, T, X, Z
 from quri_parts.qsub.link import Linker
@@ -91,6 +94,66 @@ def test_aux_qubits() -> None:
     Evaluator(hook).run(linked_msub)
 
     assert hook.result() == 5
+
+
+def test_total_qubits() -> None:
+    NS = NameSpace("test")
+    F = Op(Ident(NS, "F"), 1, 0)
+    G = Op(Ident(NS, "G"), 3, 0)
+    H = Op(Ident(NS, "H"), 2, 0)
+    K = Op(Ident(NS, "K"), 2, 0)
+
+    fb = SubBuilder(1)
+    (fq0,) = fb.qubits
+    fa0, fa1 = fb.add_aux_qubit(), fb.add_aux_qubit()  # 2 aux qubits
+    fb.add_op(G, (fq0, fa0, fa1))
+    fb.add_op(H, (fq0, fa0))  # +1 aux qubit
+    fsub = fb.build()
+
+    hb = SubBuilder(2)
+    hq0, hq1 = hb.qubits
+    ha0 = hb.add_aux_qubit()  # 1 aux qubit
+    hb.add_op(G, (hq0, hq1, ha0))
+    hb.add_op(K, (hq1, ha0))
+    hsub = hb.build()
+
+    builder = SubBuilder(2)
+    q0, q1 = builder.qubits
+    a0, a1 = builder.add_aux_qubit(), builder.add_aux_qubit()  # 2 aux qubits
+    builder.add_op(F, (q0,))  # +3 aux qubits
+    builder.add_op(H, (q1, a0))  # +1 aux qubit
+    builder.add_op(H, (a1, q0))  # +1 aux qubit
+    builder.add_op(K, (q1, a0))
+    sub = builder.build()
+
+    codegen = CodeGenerator([G, K])
+    msub = codegen.lower(sub)
+
+    calltable = {
+        F: codegen.lower(fsub),
+        H: codegen.lower(hsub),
+    }
+    linked_msub = Linker(calltable).link(msub)
+
+    hook = TotalQubitCountEvaluatorHooks()
+    Evaluator(hook).run(linked_msub)
+
+    # 2 input qubits + 5 max aux qubits
+    assert hook.result() == 7
+
+
+def test_total_same_sub_bug() -> None:
+    b = SubBuilder(4)
+    for _ in range(10):
+        b.add_op(MultiControlled(X, 3, 0b0), b.qubits)
+    sub = b.build()
+    msub = compile_sub(sub, AllBasicSet)
+
+    hook = TotalQubitCountEvaluatorHooks()
+    Evaluator(hook).run(msub)
+
+    # 4 input qubits + 2 max aux qubits
+    assert hook.result() == 6
 
 
 def test_aux_same_sub_bug() -> None:
