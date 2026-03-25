bedebf44edf28b79a25ef5d1324e07fc1e98c223
Kazutaka Homma <special18fender@gmail.com>
2026-03-25 10:37:33 +0900

    feat: add topology generation helpers to quri-parts-qret

--- quri-parts/packages/qret/quri_parts/qret/__init__.py
+++ quri-parts/packages/qret/quri_parts/qret/__init__.py
@@ -3,5 +3,17 @@ from quri_parts.qret.convert_qsub import (
     QRETInstrSet,
     create_module_from_qsub_op,
 )
+from quri_parts.qret.topology_utils import (
+    TopologySpec,
+    generate_plane_topology_yaml,
+    write_generated_topology,
+)
 
-__all__ = ["QRETInstrBaseIds", "QRETInstrSet", "create_module_from_qsub_op"]
+__all__ = [
+    "QRETInstrBaseIds",
+    "QRETInstrSet",
+    "create_module_from_qsub_op",
+    "TopologySpec",
+    "generate_plane_topology_yaml",
+    "write_generated_topology",
+]
