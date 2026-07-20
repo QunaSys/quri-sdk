70ea89b41d3c12a0e23568ebdc9fb4da67b243ed
Nils Wittemeier <nils.wittemeier@gmail.com>
2026-07-20 11:17:14 -0400

    cargo fmt

--- quri-parts/packages/rust/src/circuit/circuit.rs
+++ quri-parts/packages/rust/src/circuit/circuit.rs
@@ -200,8 +200,7 @@ impl ImmutableQuantumCircuit {
 
     #[pyo3(name = "__repr__")]
     fn py_repr<'py>(slf: &Bound<'py, Self>) -> PyResult<String> {
-        let circuit_drawer =
-            PyModule::import(slf.py(), "quri_parts.circuit.utils.circuit_drawer")?;
+        let circuit_drawer = PyModule::import(slf.py(), "quri_parts.circuit.utils.circuit_drawer")?;
         let repr = circuit_drawer.getattr("circuit_to_string")?.call1((slf,))?;
         repr.extract::<String>()
     }
